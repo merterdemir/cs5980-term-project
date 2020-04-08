588464f5a841091163ce54edc08450d7ca68d504
zamsler98 <zamsler98@gmail.com>
2020-04-08 14:46:17 -0500

    Windows 10 Update
    Updated to work on windows machines

---
+++ im2txt/run_inference.py
@@ -0,0 +1,118 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+r"""Generate captions for images using default beam search parameters."""
+
+import math
+
+import os
+
+import tensorflow as tf
+
+
+# tf.logging.set_verbosity(tf.logging.INFO)
+"""
+Make Tensorflow less verbose
+"""
+
+try:
+    # noinspection PyPackageRequirements
+    import os
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    # Monkey patching deprecation utils to shut it up! Maybe good idea to disable this once after upgrade
+    # noinspection PyUnusedLocal
+    def deprecated(date, instructions, warn_once=True):
+        def deprecated_wrapper(func):
+            return func
+        return deprecated_wrapper
+
+    from tensorflow.python.util import deprecation
+    deprecation.deprecated = deprecated
+except ImportError:
+    pass
+
+from im2txt import configuration
+from im2txt import inference_wrapper
+from im2txt.inference_utils import caption_generator
+from im2txt.inference_utils import vocabulary
+
+def generate_caption(filenames,checkpoint_path,vocab,beam_size):
+    #import sys
+    #sys.stdout = open(str(os.getpid())+".out",'a')
+    # Build the inference graph.
+    g = tf.Graph()
+    with g.as_default():
+        model = inference_wrapper.InferenceWrapper()
+        restore_fn = model.build_graph_from_config(configuration.ModelConfig(),
+                                               checkpoint_path)
+    g.finalize()
+    with tf.Session(graph = g) as sess:
+        # Load the model from checkpoint.
+        restore_fn(sess)
+
+        # Prepare the caption generator. Here we are implicitly using the default
+        # beam search parameters. See caption_generator.py for a description of the
+        # available beam search parameters.
+        result = []
+        generator = caption_generator.CaptionGenerator(model, vocab, beam_size=beam_size)
+        for filename in filenames:
+            with tf.gfile.GFile(filename, "rb") as f:
+                image = f.read()
+            captions = generator.beam_search(sess, image)
+            #print("Captions for image %s:" % os.path.basename(filename))
+            cap = []
+            for i, caption in enumerate(captions):
+              # Ignore begin and end words.
+              sentence = [vocab.id_to_word(w) for w in caption.sentence[1:-1]]
+              sentence = " ".join(sentence)
+              cap.append((sentence,math.exp(caption.logprob)))
+              #print("  %d) %s (p=%f)" % (i, sentence, math.exp(caption.logprob)))
+            result.append((filename,cap))
+    return result
+      
+
+def generate_captions(checkpoint_path,vocab_file,filenames,beam_size,multiprocessing = True, GPU = False):
+  if not GPU:
+      os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+  # Create the vocabulary.
+  vocab = vocabulary.Vocabulary(vocab_file)
+
+  
+  tf.logging.info("Running caption generation on %d files",
+                  len(filenames))
+    
+  if not multiprocessing:
+      return generate_caption(filenames,checkpoint_path,vocab,beam_size)
+  else:
+      from functools import partial
+      from multiprocessing import Pool,cpu_count
+          
+      part = partial(generate_caption,checkpoint_path = checkpoint_path,vocab = vocab,beam_size = beam_size)
+      # Splits the list a into n equal sized parts
+      def split(a, n):
+          k, m = divmod(len(a), n)
+          return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+      filenames = list(split(filenames,cpu_count()))
+      with Pool(cpu_count()) as p:
+          l = list(p.map(part, filenames))
+      result = []
+      for r in l:
+          result.extend(r)
+      return result
+    
+if __name__ == "__main__":
+  tf.app.run()
--- run.py
+++ run.py
@@ -1,99 +1,159 @@
 #!/usr/bin/env python3
 
 import os
-import sys
-import time
-import shlex
+import argparse
 import ntpath
 import logging
-import argparse
-import subprocess
-import multiprocessing
-from generate_captions import _get_filename, _validate_parameters, _parameter_parser
-
+import time
+import csv
+import json
 def _get_image_names(path):
     VALID_EXTENTIONS = ('.jpg', '.jpeg', '.png', '.gif', '.JPG', '.JPEG', '.PNG', '.GIF')
-    filenames = [f for f in os.listdir(path) if f.endswith(VALID_EXTENTIONS) and os.path.isfile(os.path.join(path, f))]
+    filenames = [os.path.join(os.getcwd(),path,f) for f in os.listdir(path) if f.endswith(VALID_EXTENTIONS) and os.path.isfile(os.path.join(path, f))]
     return filenames
 
-def _build_inference():
-    logging.info("Building inference with Bazel.")
-    bashCommand = "bazel build -c opt im2txt/run_inference"
-    process = subprocess.Popen(shlex.split(bashCommand),
-                               cwd=os.path.join(os.getcwd(), "im2txt"))
-    output, error = process.communicate()
-    if not error:
-        logging.info("Build successful!")
-        return True
-    return False
+def _get_filename(path):
+    head, tail = ntpath.split(path)
+    filename = tail or ntpath.basename(head)
+    return filename.split(".")[0]
 
-def _clean_bazel():
-    logging.info("Cleaning Bazel build.")
-    bashCommand = "bazel clean"
-    process = subprocess.Popen(shlex.split(bashCommand),
-                               cwd=os.path.join(os.getcwd(), "im2txt"))
-    output, error = process.communicate()
-    if not error:
-        process = subprocess.Popen(shlex.split("rm -rf /private/var/tmp/_bazel_*"),
-                               cwd=os.path.join(os.getcwd(), "im2txt"))
-        output, error = process.communicate()
-        if not error:
-            logging.info("Cleaned!")
-            return True
-    return False
 
-def _create_captions(args):
-    bashCommand  = "python3 generate_captions.py "
-    bashCommand += "{} ".format(args.FILE)
-    bashCommand += "-cp '{}' ".format(args.checkpoint_path)
-    bashCommand += "-vf '{}' ".format(args.vocab_file)
-    bashCommand += "-e '{}' ".format(args.export)
-    bashCommand += "-et '{}' ".format(args.export_type)
-    bashCommand += "-bs {}".format(args.beam_size)
-
-    process = subprocess.Popen(shlex.split(bashCommand), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    if not error:
-        logging.info("Success! ({})".format(_get_filename(args.FILE)))
+def _validate_parameters(args):
+    # Initialize logging
+    _id = _get_filename(args.FILE)
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s (Line %(lineno)d)")
+    logging.info("Validating parameters...")
+    if os.path.exists(args.FILE):
+        if (os.getcwd() in args.FILE):
+            logging.info("Images with id {} is ready to be captioned.".format(_id))
+        else:
+            args.FILE = os.path.join(os.getcwd(), args.FILE)
+            logging.warning("Image path changed to full path. Image {} is ready to be captioned.".format(_id))
+            logging.info("New path: {}".format(args.FILE))
     else:
-        logging.error("Error! ({})".format(_get_filename(args.FILE)))
+        logging.error("IMAGE: " + args.FILE)
+        logging.error("Given image with id {} doesn't exist! Terminating!".format(_id))
+        return False
 
-def start_captioning(args):
+    if ((os.path.exists(args.checkpoint_path) or os.path.exists(args.checkpoint_path + ".data-00000-of-00001")) and 
+       (os.path.exists(args.checkpoint_path + ".meta")) and (os.path.exists(args.checkpoint_path + ".index"))):
+        logging.info("Checkpoint file exists.")
+    else:
+        logging.error("Checkpoint file doesn't exist! It should also have index and meta files. Terminating!")
+        logging.error("Given path: {}".format(args.checkpoint_path))
+        return False
+
+    if os.path.exists(args.vocab_file):
+        logging.info("Vocabulary file exists.")
+    else:
+        logging.error("Vocabulary file doesn't exist! Terminating!")
+        return False
+
+    if os.path.exists(args.export):
+        logging.info("Export path exists.")
+    else:
+        logging.warning("Export folder doesn't exists. Creating the export folder.")
+        os.mkdir(args.export)
+    return True
+
+def _parameter_parser():
+    parser = argparse.ArgumentParser(description="Command Line Tool for generating image captions and parsing them to files.")
+
+    parser.add_argument("FILE",
+                        type=str,
+                        nargs = '?',
+                        default=os.path.join(os.getcwd(),
+                                             'images'),
+                        help="Name/Path of the corresponding Image.")
+
+    parser.add_argument("-cp", "--checkpoint_path",
+                        type=str,
+                        nargs='?',
+                        default=os.path.join(os.getcwd(),
+                                             'checkpoints',
+                                             '5M',
+                                             'model.ckpt-5000000'),
+	                    help = "Path for the checkpoint of the pretrained model.")
+
+    parser.add_argument("-vf", "--vocab_file",
+                        type=str,
+                        nargs='?',
+                        default=os.path.join(os.getcwd(),
+                                             'checkpoints',
+                                             '5M',
+                                             'word_counts.txt'),
+	                    help = "Path for the vocabulary for the evaluation.")
+
+    parser.add_argument("-e", "--export",
+                        type=str,
+                        nargs='?',
+                        default=os.path.join(os.getcwd(),
+                                             'captions'),
+	                    help = "Export folder for the parsed data of the given image (As a path).")
+
+    parser.add_argument("-et", "--export_type",
+                        type=str,
+                        nargs='?',
+                        default='csv',
+                        const='csv',
+                        choices=['csv', 'json'],
+	                    help = "Export file type of the result.")
+
+    parser.add_argument("-bs", "--beam_size",
+                        type=int,
+                        nargs='?',
+                        default=3,
+	                    help = "Number of generated captions at the end.")
+    parser.add_argument("-m","--multiprocessing",
+                        type=bool,
+                        nargs='?',
+                        default=True,
+                        help = "Run on multiple threads.")
+    parser.add_argument("-gpu","--GPU",
+                        type=bool,
+                        nargs='?',
+                        default=False,
+                        help = "Run on GPU.")
+
+    return parser.parse_args()
+
+def _export_data(data,filename,filetype,num_of_predictions):
     try:
-        logging.info("Starting to generate image captions.")
-        with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
-            p.map(_create_captions, args)
-        logging.info("All processes are finalized!")
-    except:
-        logging.error("Error while initializing the processes.")
-
+        logging.info("Exporting captions to {}.".format(filetype))
+        with open(os.path.join(filename+"."+filetype),"w") as f:
+            if (filetype == 'csv'):
+                fieldnames = ['id']
+                for i in range(num_of_predictions):
+                    fieldnames.append("prediction{}".format(i))
+                    fieldnames.append("logprob{}".format(i))
+                csvFile = csv.writer(f)
+                csvFile.writerow(fieldnames)
+                for d in data:
+                    row = [os.path.splitext(os.path.basename(d[0]))[0]]
+                    for cap in d[1]:
+                        row.extend(cap)
+                    csvFile.writerow(row)
+            else:
+                f.write(json.dumps(data))
+    except Exception as e:
+        logging.error("Error while exporting captions: "+str(e))
+        return False
+    
 def main(args):
-    if (_validate_parameters(args)):
-        images  = _get_image_names(args.FILE)
-        img_cnt = len(images)
-        logging.info("Number of Images: {}".format(img_cnt))
-
-        if not _build_inference():
-            logging.error("Couldn't build bazel! Try again. Terminating!")
-            sys.exit(-1)
-
-        all_args = []
-        for image in images:
-            if image:
-                new_args = argparse.Namespace(**vars(args))
-                new_args.FILE = os.path.join(args.FILE, image)
-                all_args.append(new_args)
-
-        st = time.time()
-        start_captioning(all_args)
-        et = time.time()
-        logging.info("Task completed in {} seconds!".format(et - st))
-        if not _clean_bazel():
-            logging.error("Couldn't clean the bazel build! Try again. Terminating!")
-            sys.exit(-1)
-
-
+    from im2txt import run_inference
+    filenames = _get_image_names(args.FILE)
+    checkpoint_path = os.path.join(os.getcwd(), args.checkpoint_path)
+    vocab_path = os.path.join(os.getcwd(), args.vocab_file)
+    beam_size = args.beam_size
+    logging.info("Generating Captions for {} images".format(len(filenames)))
+    start = time.time()
+    m = run_inference.generate_captions(checkpoint_path,vocab_path,filenames,beam_size,multiprocessing = args.multiprocessing, GPU = args.GPU)
+    logging.info("Done!")
+    logging.info("Generating captions for {} images took {:.2f} seconds".format(len(filenames),time.time()-start))
+    _export_data(m,args.export,args.export_type,args.beam_size)
+    input()
 
 if (__name__ == "__main__"):
     args = _parameter_parser()
-    main(args)
+    if _validate_parameters(args):
+        main(args)
