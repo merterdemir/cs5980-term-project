81510d08b3d522ec11d3838705108f3099d3d69a
AnKeil03 <61359100+AnKeil03@users.noreply.github.com>
2020-04-22 20:37:35 -0500

    added csv to txt file converter
    added csv to txt file converter for generated image captions

---
+++ csv_to_txt.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def readFile():
+    fileName = input("Enter the file name + extension to open: ")
+    col_list = ["prediction0","prediction1","prediction2"]
+    df = pd.read_csv(fileName,usecols = col_list)
+    
+    for i in range(0,3):
+        p = df["prediction"+str(i)]
+        output = open("prediction"+str(i)+".txt","w")
+        for r in p:
+            r = r.strip(".")
+            output.write(str(r)+"\n")
+        output.close()
+        
+def main():
+    readFile()
+    
+if __name__ == '__main__':
+    main()
+    
